ad8e616e03e97256552f619804aba9845f733420
abc631626830 <a631626830@gmail.com>
2019-09-02 10:26:04 +0800

    added API key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+const APIKEY = "7e15c664ea87f151a3057bc0afb65e49";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
